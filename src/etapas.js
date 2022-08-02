c53af181bb563cfdf2a4e95c7b205cdcebce8b84
Cicero Junior <cicerojunior1011@hotmail.com>
2022-08-02 00:29:09 -0300

    criei o arquivo etapas.js
    e adicionei outras imagens

---
+++ src/etapas.js
@@ -0,0 +1,40 @@
+let etapas = [{
+    titulo: 'VEREADOR',
+    numero: 5,
+    candidatos: [{
+        numero: 38111,
+        nome: 'Fulano de Tal',
+        partido: 'ABC',
+        fotos: [{ url: 'Zhongli_icon.webp', legenda: 'Vereador'}]
+    }, 
+    {
+        numero: 77222,
+        nome: 'Beltrano de Tal',
+        partido: 'DEFG',
+        fotos: [{ url: 'Yoimiya_icon.webp', legenda: 'Vereador' }]
+    },]
+}, 
+{
+    titulo: 'PREFEITO',
+    numero: 2,
+    candidatos: [{
+        numero: 99,
+        nome: 'Ciclano Cilada',
+        partido: 'ABC',
+        vice: 'Cic',
+        fotos: [
+            { url: 'Yelan_icon.webp', legenda: 'Prefeito' },
+            { url: 'Shenhe_icon.webp', legenda: 'Vice-Prefeito', small:true }
+        ]
+    }, 
+    {
+        numero: 84,
+        nome: 'Juscelino Kubracheka',
+        partido: 'JAVA',
+        vice: 'Thaka',
+        fotos: [
+            { url: 'RaidenShogun_icon.webp', legenda: 'Prefeito' },
+            { url: 'Xiao_icon.webp', legenda: 'Vice-Prefeito', small: true }
+        ]            
+    }]
+}]
