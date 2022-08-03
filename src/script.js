f66cff9a0b75b13bf36b7022ba5d5d4c374fa889
Cicero Junior <cicerojunior1011@hotmail.com>
2022-08-03 14:42:58 -0300

    projeto completado

--- src/script.js
+++ src/script.js
@@ -5,25 +5,33 @@ let aviso = document.querySelector('.d-2')
 let lateral = document.querySelector('.d-1-right')
 let numeros = document.querySelector('.d-1-3')
 
+
+
 let etapaAtual = 0
 let numero = ''
+let votoBranco = false
+let votos = []
+let votoConfirmado = false
 
 function comecarEtapa() {
     let etapa = etapas[etapaAtual]
 
     let numeroHMTL = ''
+    numero = ''
+    votoBranco = false
 
     for(let i=0;i<etapa.numeros;i++) {
         if(i === 0) {
             numeroHMTL += '<div class="numero pisca"></div>'
+        } else {
+            numeroHMTL += '<div class="numero"></div>'
         }
-        numeroHMTL += '<div class="numero"></div>'
     }
 
     seuVotoPara.style.display = 'none'
     cargo.innerHTML = etapa.titulo
     descricao.innerHTML = ''
-    aviso.style.display = 'nome'
+    aviso.style.display = 'none'
     lateral.innerHTML = ''
     numeros.innerHTML = numeroHMTL
 }
@@ -36,39 +44,87 @@ function atualizaInterface() {
             return false
         }
     })
-    if(candidato.length === 0) {
+    
+    if(candidato.length > 0) {
         candidato = candidato[0]
         seuVotoPara.style.display = 'block'
         aviso.style.display = 'block'
         descricao.innerHTML = `Nome: ${candidato.nome}<br/> Partido: ${candidato.partido}`
 
         let fotosHMTL = ''
-
+        for(let i in candidato.fotos) {
+            if(candidato.fotos[i].small) {
+                fotosHMTL += `<div class="d-1-image small"><img src="images/${candidato.fotos[i].url}" />${candidato.fotos[i].legenda} </div>`
+            } else {
+                fotosHMTL += `<div class="d-1-image"><img src="images/${candidato.fotos[i].url}" />${candidato.fotos[i].legenda} </div>`
+            }
+        }
         lateral.innerHTML = fotosHMTL
+    } else {
+        seuVotoPara.style.display = 'block'
+        aviso.style.display = 'block'
+        descricao.innerHTML = '<div class="aviso--grande pisca"> VOTO NULO </div>'
     }
 }
 
 function clicou(n) {
-    let elementoNumero = document.querySelector('.numero.pisca')
-    if(elementoNumero !== null) {
-        elementoNumero.innerHTML = n
+    let elNumero = document.querySelector('.numero.pisca')
+    if(elNumero !== null) {
+        elNumero.innerHTML = n
         numero = `${numero}${n}`
 
-        elementoNumero.classList.remove('pisca')
-        if(elementoNumero.nextElementSibling !== null) {
-            elementoNumero.nextElementSibling.classList.add('pisca')
+        elNumero.classList.remove('pisca')
+        if(elNumero.nextElementSibling !== null) {
+            elNumero.nextElementSibling.classList.add('pisca')
         } else {
             atualizaInterface()
         }
     }
 }
 function branco() {
-    alert("Clicou em BRANCO!")
+    numero = ''
+    votoBranco = true
+    seuVotoPara.style.display = 'block'
+    aviso.style.display = 'block'
+    numeros.innerHTML = ''
+    lateral.innerHTML = ''
+    descricao.innerHTML = '<div class="aviso--grande pisca"> VOTO EM BRANCO </div>'
 }
 function corrige() {
-    alert("Clicou em CORRIGE!")
+    if(votoConfirmado) {
+
+    } else {
+        comecarEtapa()
+    }
 }
 function confirma() {
-    alert("Clicou em CONFIRMA!")
+    let etapa = etapas[etapaAtual]
+    
+    if(votoBranco === true) {
+        votoConfirmado = true
+        votos.push({
+            etapa: etapas[etapaAtual].titulo,
+            voto: 'branco'
+        })
+    } else if(numero.length === etapa.numeros) {
+        votoConfirmado = true
+        votos.push({
+            etapa: etapas[etapaAtual].titulo,
+            voto: numero
+        })
+    }
+
+    if(votoConfirmado) {
+        etapaAtual++
+        if(etapas[etapaAtual] !== undefined) {
+            comecarEtapa()
+            votoConfirmado = false
+        } else {
+            document.querySelector('.tela').innerHTML = '<div class="aviso--gigante"> FIM </div>'
+            
+            console.log(votos)
+        }
+
+    }
 }
 comecarEtapa()
